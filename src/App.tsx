220cba3e11c4fbe8ad9279869a0a53d6ac0ed15f
bence <benceluzsinszky@gmail.com>
2024-08-12 20:48:16 +0200

    autoformatter

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import './App.css';
+import "./App.css";
 import HomeButton from "./components/common/HomeButton";
-import GameOfLife from './components/game_of_life/GameOfLife';
-import HomePage from './components/home/HomePage';
+import GameOfLife from "./components/game_of_life/GameOfLife";
+import HomePage from "./components/home/HomePage";
+import Placeholder from "./components/placeholder/Placeholder";
 
 export default function App() {
-
   return (
     <>
       <BrowserRouter>
-      <HomeButton />
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/game-of-life' element={<GameOfLife />} />
-      </Routes>
+        <HomeButton />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/game-of-life" element={<GameOfLife />} />
+          <Route path="/placeholder" element={<Placeholder />} />
+        </Routes>
       </BrowserRouter>
     </>
   );
-};
+}
