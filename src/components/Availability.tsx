7e31a28455ab3968572ef42e75ff019a2570748b
bence <benceluzsinszky@gmail.com>
2025-01-08 20:57:54 +0100

    add availability

---
+++ src/components/Availability.tsx
@@ -0,0 +1,8 @@
+export default function Availability() {
+  return (
+    <div className="inline-flex flex-row items-center border rounded-full py-2 px-3">
+      <div className="bg-green-500 rounded-full size-2 mr-2"></div>
+      <p>Looking for work</p>
+    </div>
+  );
+}
--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -1,10 +1,11 @@
+import Availability from "../../components/Availability";
 import GithubContributionChart from "../../components/githubContributionChart/GithubContributionChart";
 
 export default function HomePage() {
   return (
     <div>
+      <Availability />
       <h1>Hi, I'm Bence</h1>
-      <h1>verjem ala</h1>
       <h2>And this is my portfolio</h2>
       <p>I am a software engineer with</p>
       <GithubContributionChart />
