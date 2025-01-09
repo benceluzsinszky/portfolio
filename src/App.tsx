38be67e75cda402b63b3eea0ceb3cb5a7f861a38
bence <benceluzsinszky@gmail.com>
2025-01-09 14:33:56 +0100

    add html open-close

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { MantineProvider } from "@mantine/core";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import HtmlCloseIcon from "./assets/icons/html_close.svg";
+import HtmlOpenIcon from "./assets/icons/html_open.svg";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HomePage from "./pages/homePage/HomePage";
@@ -11,9 +13,21 @@ function App() {
         <div className="flex flex-col min-h-screen items-center">
           <Header />
           <main className="flex-grow mb-auto w-10/12">
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-            </Routes>
+            <img
+              src={HtmlOpenIcon}
+              alt="HTML open tag"
+              className="w-20 h-20 mb-5"
+            />
+            <div className="mx-14">
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+              </Routes>
+            </div>
+            <img
+              src={HtmlCloseIcon}
+              alt="HTML close tag"
+              className="w-20 h-20 mt-5"
+            />
           </main>
           <Footer />
         </div>
