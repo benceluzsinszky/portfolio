0d158d8f1d19ad3afd4713ac7b6ee7161c9748e3
bence <benceluzsinszky@gmail.com>
2025-01-09 14:33:31 +0100

    update availability text

--- src/components/Availability.tsx
+++ src/components/Availability.tsx
@@ -3,7 +3,7 @@ export default function Availability() {
     <div className="inline-flex flex-row items-center border rounded-full py-1 px-3">
       <div className="absolute size-3 animate-ping rounded-full bg-green-300 opacity-65"></div>
       <div className="mr-2 size-3 rounded-full bg-green-400 drop-shadow-green"></div>
-      <p>Looking for work</p>
+      <p>Open to Work</p>
     </div>
   );
 }
