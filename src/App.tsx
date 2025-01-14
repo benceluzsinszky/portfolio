9ace11a76ee3697ef5c6f07262f5af0d515b8a6b
bence <benceluzsinszky@gmail.com>
2025-01-14 16:22:47 +0100

    style html tags

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { MantineProvider } from "@mantine/core";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import HtmlCloseIcon from "./assets/icons/html_close.svg";
 import HtmlOpenIcon from "./assets/icons/html_open.svg";
 import Footer from "./components/Footer";
@@ -12,21 +12,24 @@ function App() {
       <Router>
         <div className="flex flex-col min-h-screen items-center">
           <Header />
-          <main className="flex-grow mb-auto w-10/12">
-            <img
-              src={HtmlOpenIcon}
-              alt="HTML open tag"
-              className="w-20 h-20 mb-5"
-            />
+          <main className="flex-grow mb-auto w-10/12 relative">
+            <Link to="/">
+              <img
+                src={HtmlOpenIcon}
+                alt="HTML open tag"
+                className="w-20 h-20 mb-5"
+              />
+            </Link>
             <div className="mx-14">
               <Routes>
                 <Route path="/" element={<HomePage />} />
               </Routes>
             </div>
+            <div className="h-20 mt-5"></div>
             <img
               src={HtmlCloseIcon}
               alt="HTML close tag"
-              className="w-20 h-20 mt-5"
+              className="w-20 h-20 mt-5 absolute bottom-0"
             />
           </main>
           <Footer />
