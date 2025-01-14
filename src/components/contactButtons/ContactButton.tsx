8f854d4f8ddf74764c5bf0c6d1a71484bf5f7e1a
bence <benceluzsinszky@gmail.com>
2025-01-14 16:23:08 +0100

    change button styling

--- src/components/contactButtons/ContactButton.tsx
+++ src/components/contactButtons/ContactButton.tsx
@@ -10,7 +10,7 @@ export default function ContactButton({ href, icon, alt }: ContactButtonProps) {
   return (
     <Link
       to={href}
-      className="transform transition-transform brightness-90 duration-100 hover:brightness-110 hover:scale-105 active:translate-y-1"
+      className="transform transition-transform brightness-90 duration-100 hover:brightness-110 hover:scale-110 active:translate-y-1"
       title={alt}
     >
       <img src={icon} alt={alt} className="w-10 h-10" />
