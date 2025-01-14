1aa45eb2547a39d0643d0d9247067db400320ff6
bence <benceluzsinszky@gmail.com>
2025-01-14 21:22:02 +0100

    update styling

--- src/components/Availability.tsx
+++ src/components/Availability.tsx
@@ -4,7 +4,7 @@ export default function Availability() {
   return (
     <div className="inline-flex flex-row items-center border rounded-full py-1 px-3">
       <SignalLight />
-      <p>Open to Work</p>
+      <p className="text-sm">Open to Work</p>
     </div>
   );
 }
