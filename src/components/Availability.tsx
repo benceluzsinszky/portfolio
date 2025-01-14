bf527fe70e86afad423250991c53ad3c05a1c335
bence <benceluzsinszky@gmail.com>
2025-01-14 16:30:31 +0100

    export SignalLight component

--- src/components/Availability.tsx
+++ src/components/Availability.tsx
@@ -1,8 +1,9 @@
+import SignalLight from "./SignalLight";
+
 export default function Availability() {
   return (
     <div className="inline-flex flex-row items-center border rounded-full py-1 px-3">
-      <div className="absolute size-3 animate-ping rounded-full bg-green-300 opacity-65"></div>
-      <div className="mr-2 size-3 rounded-full bg-green-400 drop-shadow-green"></div>
+      <SignalLight />
       <p>Open to Work</p>
     </div>
   );
---
+++ src/components/SignalLight.tsx
@@ -0,0 +1,8 @@
+export default function SignalLight() {
+  return (
+    <div>
+      <div className="absolute size-3 animate-ping rounded-full bg-green-300 opacity-65"></div>
+      <div className="mr-2 size-3 rounded-full bg-green-400 drop-shadow-green"></div>
+    </div>
+  );
+}
