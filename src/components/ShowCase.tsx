c49167f4e506373389605e1044964310626f5d36
bence <benceluzsinszky@gmail.com>
2025-01-16 14:25:16 +0100

    add children element to showcase

--- src/components/ShowCase.tsx
+++ src/components/ShowCase.tsx
@@ -1,19 +1,19 @@
 type ShowCaseProps = {
-  icon: string;
   title: string;
-  element: JSX.Element;
+  children: React.ReactNode;
+  icon?: string;
 };
 
-export default function ShowCase({ icon, title, element }: ShowCaseProps) {
+export default function ShowCase({ title, children, icon }: ShowCaseProps) {
   return (
     <div className="flex flex-col min-h-full overflow-hidden flex-1 border p-5 rounded-3xl bg-zinc-950">
       <div>
         <div className="inline-flex items-center mb-3 border rounded-full py-1 px-3 w-auto bg-gray-950">
-          <img src={icon} alt={title} className="w-6" />
+          {icon && <img src={icon} alt={title} className="w-6" />}
           <p className="mx-2 text-sm">{title}</p>
         </div>
       </div>
-      {element}
+      {children}
     </div>
   );
 }
