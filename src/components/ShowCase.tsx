6e18e5d3ee5f55b405afbfcd958931401b729504
bence <benceluzsinszky@gmail.com>
2025-01-16 15:09:32 +0100

    force icon in showcase

--- src/components/ShowCase.tsx
+++ src/components/ShowCase.tsx
@@ -1,16 +1,16 @@
 type ShowCaseProps = {
   title: string;
   children: React.ReactNode;
-  icon?: string;
+  icon: string;
 };
 
 export default function ShowCase({ title, children, icon }: ShowCaseProps) {
   return (
     <div className="flex flex-col min-h-full overflow-hidden flex-1 border p-5 rounded-3xl bg-zinc-950">
       <div>
-        <div className="inline-flex items-center mb-3 border rounded-full py-1 px-3 w-auto bg-gray-950">
-          {icon && <img src={icon} alt={title} className="w-6" />}
-          <p className="mx-2 text-sm">{title}</p>
+        <div className="inline-flex items-center mb-3 border rounded-full py-2 px-4 w-auto bg-gray-950">
+          <img src={icon} alt={title} className="w-6 rounded-md" />
+          <p className="mx-2 text-sm text-nowrap">{title}</p>
         </div>
       </div>
       {children}
