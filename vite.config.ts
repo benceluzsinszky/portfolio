26bbf3ebed1e7a82b68e7f5254a866f16e8cb316
bence <benceluzsinszky@gmail.com>
2025-01-18 20:01:47 +0100

    remove unused dependenceis

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,4 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    VITE_GITHUB_PAT: JSON.stringify(process.env.VITE_GITHUB_PAT),
-  },
 });
