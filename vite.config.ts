10b0cbc6603349661f1f790e9f67aa2a48e19497
bence <benceluzsinszky@gmail.com>
2025-01-18 19:59:08 +0100

    dependencies for env vars

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    VITE_GITHUB_PAT: JSON.stringify(process.env.VITE_GITHUB_PAT),
+  },
+});
