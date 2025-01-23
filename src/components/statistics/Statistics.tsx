c03d2d98f832a1b842febc3d9c33f4013b0a87d7
bence <benceluzsinszky@gmail.com>
2025-01-23 17:26:15 +0100

    remove consoloe log

--- src/components/statistics/Statistics.tsx
+++ src/components/statistics/Statistics.tsx
@@ -43,7 +43,6 @@ export default function Statistics() {
       await axiosConfig
         .get("/total_lines")
         .then((response) => {
-          console.log(response.data);
           setLinesOfCode(response.data.total_lines);
         })
         .catch((error) => {
