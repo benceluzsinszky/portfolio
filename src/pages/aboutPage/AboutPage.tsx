1dd8a192e2f9ae17c04eab52d058b091b23abb2d
bence <benceluzsinszky@gmail.com>
2025-01-24 15:46:12 +0100

    add meta

--- src/pages/aboutPage/AboutPage.tsx
+++ src/pages/aboutPage/AboutPage.tsx
@@ -1,6 +1,37 @@
+import { Helmet } from "react-helmet-async";
+import ogImage from "../../assets/og_image.png";
+
 export default function AboutPage() {
   return (
     <div className="space-y-5">
+      <Helmet>
+        <title>About - Bence Luzsinszky</title>
+        <meta
+          name="description"
+          content="Welcome to my portfolio. Full Stack Developer with a background in Electrical Engineering."
+        />
+        <meta
+          name="keywords"
+          content="Bence, Luzsisnzky, benceluzsisnzky, Portfolio, Full Stack Developer, Electrical Engineering, Software Design, ITU"
+        />
+        <meta property="og:title" content="About - Bence Luzsinszky" />
+        <meta
+          property="og:description"
+          content="Welcome to my portfolio. Full Stack Developer with a background in Electrical Engineering."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://benceluzsinszky.com" />
+        <meta property="og:image" content={ogImage} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="About - Bence Luzsisnzky" />
+        <meta
+          name="twitter:description"
+          content="Welcome to my portfolio. Full Stack Developer with a background in Electrical Engineering."
+        />
+        <meta name="twitter:image" content={ogImage} />
+        <meta name="author" content="Bence Luzsisnzky" />
+        <meta name="robots" content="index, follow" />
+      </Helmet>
       <h1>Portfolio</h1>
       <p>
         The aim of the project is to showcase my experiences and projects in a
