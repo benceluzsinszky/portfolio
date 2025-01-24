823aed89b52297cde926db44c74fade25ed47f9b
bence <benceluzsinszky@gmail.com>
2025-01-24 15:51:27 +0100

    add bout to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+
 export default function Footer() {
   return (
-    <footer className="h-10">
+    <footer className="inline-flex items-center h-10 space-x-3">
       <p>&copy; 2025 Bence Luzsinszky. All rights reserved.</p>
+      <p>&middot;</p>
+      <Link className="underline" to="/about">
+        About
+      </Link>
     </footer>
   );
 }
