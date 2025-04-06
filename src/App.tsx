192399e065c9b3cf465a17bf2a1ba8d0aa57861e
Bence <benceluzsinszky@gmail.com>
2025-04-06 16:39:36 +0200

    add game of life

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { MantineProvider } from "@mantine/core";
 import { Helmet, HelmetProvider } from "react-helmet-async";
-import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import HtmlCloseIcon from "./assets/icons/html_close.svg";
-import HtmlOpenIcon from "./assets/icons/html_open.svg";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import AboutPage from "./pages/aboutPage/AboutPage";
 import CurriculumVitae from "./pages/cvPage/CurriculumVitae";
 import FallingSand from "./pages/games/fallingSand/FallingSand";
+import GameOfLife from "./pages/games/gameOfLife/GameOfLife";
 import HomePage from "./pages/homePage/HomePage";
 
 function App() {
@@ -26,28 +25,17 @@ function App() {
           <div className="flex flex-col min-h-screen items-center !scroll-smooth">
             <Header />
             <main className="flex-grow mb-auto max-w-full md:w-10/12 relative">
-              <Link to="/">
-                <img
-                  src={HtmlOpenIcon}
-                  alt="HTML open tag"
-                  className="w-20 h-20 mb-5 ml-3 md:ml-0"
-                />
-              </Link>
               <div className="mx-4 sm:mx-8 md:mx-14">
                 <Routes>
                   <Route path="/" element={<HomePage />} />
                   <Route path="/cv.pdf" element={<CurriculumVitae />} />
                   <Route path="/about" element={<AboutPage />} />
-                  <Route path="/falling_sand" element={<FallingSand />} />
+                  <Route path="/games/falling_sand" element={<FallingSand />} />
+                  <Route path="/games/game_of_life" element={<GameOfLife />} />
                   <Route path="*" element={<HomePage />} />
                 </Routes>
               </div>
               <div className="h-20 mt-5"></div>
-              <img
-                src={HtmlCloseIcon}
-                alt="HTML close tag"
-                className="w-20 h-20 mt-5 absolute bottom-0  ml-3 md:ml-0"
-              />
             </main>
             <Footer />
           </div>
